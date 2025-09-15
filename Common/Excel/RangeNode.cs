ca6badcc6fa6917abf01068b5bbf256a92a43d93
John Ryan Taylor <the.taylor.john.ryan@gmail.com>
2025-09-14 21:22:23 -0400

    Update RangeNode.cs
    Added documentation

--- Common/Excel/RangeNode.cs
+++ Common/Excel/RangeNode.cs
@@ -3,6 +3,9 @@ using Common.Regex;
 using XlRange = Microsoft.Office.Interop.Excel.Range;
 
 namespace Common.Excel;
+/// <summary>
+/// A node within a range.
+/// </summary>
 public class RangeNode : IEquatable<RangeNode>, IComparable<RangeNode>, IGeneratable<MatchData, RangeNode>
 {
   #region Static Members
@@ -12,13 +15,24 @@ public class RangeNode : IEquatable<RangeNode>, IComparable<RangeNode>, IGenerat
   #endregion
 
   #region Public Properties
+  /// <summary>
+  /// The containing worksheet.
+  /// </summary>
   public string Sheet { get; set; } = SE;
   public string Address { get; set; } = SE;
   public bool RowAbs { get; set; }
+  /// <summary>
+  /// The row index.
+  /// </summary>
   public int? Row { get; set; }
   public bool ColAbs { get; set; }
+  /// <summary>
+  /// The column index;
+  /// </summary>
   public string? Col { get; set; }
-
+  /// <summary>
+  /// Whether or not this is the whole row or not.
+  /// </summary>
   public bool WholeRow { get; set; }
   public bool WholeCol { get; set; }
 
