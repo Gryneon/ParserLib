779dca8cfd9975865a24feb597b23ede3800b529
John Ryan Taylor <querpus@gmail.com>
2026-03-07 23:27:47 -0500

    dd

--- Specification.XML/Globals.cs
+++ Specification.XML/Globals.cs
@@ -1,4 +1,6 @@
 global using System.Collections.ObjectModel;
+global using System.Reflection;
+global using System.Resources;
 
 global using Common;
 global using Common.Extensions;
@@ -6,3 +8,11 @@ global using Common.Regex;
 
 global using static Common.Names;
 
+[assembly: AssemblyCompany("Specification.XML")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-Prerelease")]
+[assembly: AssemblyProduct("Specification.XML")]
+[assembly: AssemblyTitle("Specification.XML")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
