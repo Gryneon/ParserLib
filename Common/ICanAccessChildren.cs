112b8a6009ab9d037d7621d0c7b5eaf5650cd2d3
John Ryan Taylor <querpus@gmail.com>
2026-08-14 18:34:02 -0400

    gg

--- Common/ICanAccessChildren.cs
+++ Common/ICanAccessChildren.cs
@@ -2,9 +2,15 @@
 
 namespace Common;
 
+/// <summary>Provides a simple keyed lookup framework.</summary>
+/// <typeparam name="TIndex">The index or key type.</typeparam>
+/// <typeparam name="TChild">The child's type.</typeparam>
 public interface ICanAccessChildren<TIndex, out TChild> : IEnumerable<TChild> where TIndex : notnull
 {
   /// <summary>The number of children in this object.</summary>
   int Count { get; }
+  /// <summary>Accesses the child stored at <paramref name="index"/>.</summary>
+  /// <param name="index">The index or key to lookup.</param>
+  /// <returns>The <typeparamref name="TChild"/> at <paramref name="index"/>.</returns>
   TChild this[TIndex index] { get; }
 }
